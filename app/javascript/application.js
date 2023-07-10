df5332d9b2c92c354ac79b5063dde9cfe2ecbf20
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-07-10 16:42:51 -0500

    Create first component without routes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,18 @@
+import "@hotwired/turbo-rails";
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { Route, Routes } from 'react-router-dom';
+// import Message from "./message";
 
 function App() {
     return (<h1>Hello World!</h1>);
 }
 
 ReactDOM.render(
+    // <Routes>
+    //     <Route exact path="/" component={App} />
+    //     <Route exact path="/greeting" component={Message} />
+    // </Routes>,
     <App />,
     document.getElementById('root'),
 );
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root 'root#index'
-
-  get 'root/index'
+  get 'greeting/index'
 end
